0cbdb41d2595a19a763ff3051c950e3a94faff62
Marc <marc.j.bouwman@gmail.com>
2018-06-23 19:16:30 +0200

    Use themoviedb.org

--- routes/v1/api.js
+++ routes/v1/api.js
@@ -1,10 +1,31 @@
+const request = require('request');
 const express = require("express")
 const router = express.Router()
 const dm = require("../../data/manager")
 const dataManager = new dm()
 
-router.route("/movies").get((request, response) => {
-    response.status(200).json(dataManager.getAllMovies())
+router.route("/movies").get((req, res) => {
+    const moviesIDlist = dataManager.getAllMovies()
+    const movieData = []
+    moviesIDlist.forEach((movieID) => {
+        request({
+            url: `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.TMDB_KEY}&language=nl-NL`,
+            method: "GET"
+        }, function (error, response, body){
+            if (!error && response.statusCode == 200) {
+                var movie = JSON.parse(body);
+                movie.poster = "https://image.tmdb.org/t/p/w500/" + movie.poster_path
+                movie.description = movie.overview
+                movieData.push(movie)
+            } else {
+                movieData.push({
+                    id: movieID
+                })
+            }
+            
+            if(moviesIDlist.length === movieData.length) res.status(200).json(movieData)
+        });
+    })
 })
 
 router.route("/movies/:ID?").get((request, response) => {
