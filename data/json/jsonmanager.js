4d67add9534f0fe0612cc27287ff122f2cd50899
Marc <marc.j.bouwman@gmail.com>
2018-06-24 13:56:40 +0200

    Rewrite API

--- data/json/jsonmanager.js
+++ data/json/jsonmanager.js
@@ -2,27 +2,18 @@ require('json5/lib/register')
 
 const cinemas = require("./cinemas.json5")
 const halls = require("./halls.json5")
-const movies = require("./movies.json5")
 const shows = require("./shows.json5")
 
 module.exports = class {
-    getAllMovies(){
-        return movies
-    }
-
     getAllShows(){
         return shows
     }
 
-    getHallById(id){
-        return halls.filter((hall) => hall.id === id)
+    getHalls(){
+        return halls
     }
 
-    getCinemaById(id){
-        return cinemas.filter((cinema) => cinema.id === id)
-    }
-
-    getMovieById(id){
-        return movies.filter((movie) => movie.id === id)
+    getCinemas(){
+        return cinemas
     }
 }
--- routes/v1/api.js
+++ routes/v1/api.js
@@ -1,50 +1,69 @@
-const request = require('request');
+const httpRequest = require('request');
 const express = require("express")
 const router = express.Router()
-const dm = require("../../data/manager")
-const dataManager = new dm()
+const dataManager = new (require("../../data/manager"))()
 
-router.route("/movies").get((req, res) => {
-    const moviesIDlist = dataManager.getAllMovies()
+Array.prototype.removeDuplicates = function(){
+    return [...new Set(this)]
+}
+
+function getMovieDataById(movieID, callback){
+    httpRequest({
+        url: `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.TMDB_KEY}&language=nl-NL`,
+        method: "GET"
+    }, function (error, response, body){
+        if (!error && response.statusCode == 200) {
+            var movie = JSON.parse(body);
+            movie.poster = "https://image.tmdb.org/t/p/w780" + movie.poster_path
+            movie.backdrop = "https://image.tmdb.org/t/p/w1280" + movie.backdrop_path
+            callback(null, movie)
+        } else callback(error, null)
+    })
+}
+
+function mapMovieToDisplayType(displayType, movie){
+    if(displayType == "poster") return {
+        id: movie.id,
+        poster: movie.poster,
+        title: movie.title
+    }
+
+    if(displayType == "details") return {
+        id: movie.id,
+        backdrop: movie.backdrop,
+        title: movie.title,
+        overview: movie.overview
+    }
+    
+    return movie
+}
+
+router.route("/shows/movies").get((request, response) => {
+    const displayType = request.query.displayType
+    const moviesIDlist = dataManager.getAllShows().map((show) => show.movieID).removeDuplicates()
+    let moviesLeft = moviesIDlist.length
     const movieData = []
     moviesIDlist.forEach((movieID) => {
-        request({
-            url: `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.TMDB_KEY}&language=nl-NL`,
-            method: "GET"
-        }, function (error, response, body){
-            if (!error && response.statusCode == 200) {
-                var movie = JSON.parse(body);
-                movie.poster = "https://image.tmdb.org/t/p/w500" + movie.poster_path
-                movie.description = movie.overview
-                movie.name = movie.title
-                movie.minAge = movie.adult ? 18 : 12
-                movie.duration = movie.runtime
-                movieData.push(movie)
-            } else {
-                movieData.push({
-                    id: movieID
-                })
-            }
-            
-            if(moviesIDlist.length === movieData.length) res.status(200).json(movieData)
-        });
+        getMovieDataById(movieID, (error, movie) => {
+            if(!error) movieData.push(mapMovieToDisplayType(displayType, movie))
+            moviesLeft--
+            if(moviesLeft === 0) response.status(200).json(movieData)
+        })
     })
 })
 
 router.route("/movies/:ID?").get((request, response) => {
-    response.status(200).json(dataManager.getMovieById(parseInt(request.params.ID)))
+    const displayType = request.query.displayType
+    getMovieDataById(parseInt(request.params.ID), (error, movie) => {
+        if(error) response.status(400).json({
+            title: "Error fetching movie."
+        });
+        else response.status(200).json(mapMovieToDisplayType(displayType, movie))
+    })
 })
 
 router.route("/shows").get((request, response) => {
     response.status(200).json(dataManager.getAllShows())
-})
-
-router.route("/halls/:ID?").get((request, response) => {
-    response.status(200).json(dataManager.getHallById(parseInt(request.params.ID)))
-})
-
-router.route("/cinemas/:ID?").get((request, response) => {
-    response.status(200).json(dataManager.getCinemaById(parseInt(request.params.ID)))
 })
 
 module.exports = router
